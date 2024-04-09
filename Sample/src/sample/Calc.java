92dac10d5076e752b6c2320f1f24b17ec43c40e1
dir-FY24yoshidatatsuya <TatsuyaYoshida()@Dirbato-1341x>
2024-04-09 09:33:31 +0900

    フェッチ

--- Sample/src/sample/Calc.java
+++ Sample/src/sample/Calc.java
@@ -6,7 +6,7 @@ public class Calc {
         var num1 = 100;
         var num2 = 10;
         
-        //足し算
+        //足し算をさせる
         var result1 = num1 + num2;
         System.out.println("num1 + num2 = " + result1);
         
